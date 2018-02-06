69ee7c4b11df4b6a7b25ee952e23fb19031cf1a3
Nick Hathaway <Nicholashathaway@gmail.com>
2018-02-06 18:56:28 -0500

    scraper on small set of 10 reviews

--- idsToAllReviews.js
+++ idsToAllReviews.js
@@ -1,20 +1,60 @@
-// let numReviews = 0
-//   , paginationKey = ''
-//   , baseUrl = `http://www.imdb.com/title/${req.params.id}/reviews/_ajax`
-//   , reviewsTXT = '';
+require('dotenv').config();
+const axios = require('axios')
+    , fs = require('fs')
+    , cheerio = require('cheerio');
 
-const fs = require('fs');
+let titleDirectory = './reviews-genre/titles-with-ids';
+let reviewDirectory = './reviews-genre/all-reviews';
+let genre = 'horror';
+// let genres = ['horror', 'comedy', 'animation', 'romance', 'documentary'];
 
-fs.mkdir('./movie-reviews-by-genre', '0o777', err => console.log(err));
-fs.mkdir('./movie-reviews-by-genre/animation', '0o777', err => console.log(err));
-fs.mkdir('./movie-reviews-by-genre/animation/pos', '0o777', err => console.log(err));
-fs.mkdir('./movie-reviews-by-genre/animation/neg', '0o777', err => console.log(err));
+// fs.mkdir(`${reviewDirectory}`, '0o777', err => console.log(err));
+// fs.mkdir(`${reviewDirectory}/${genre}`, '0o777', err => console.log(err));
 
-fs.readFile('./intermediate-csv/animation_imdb_ids.txt', 'utf-8', (err, data) => {
+fs.readFile(`${titleDirectory}/${genre}_titles_with_ids.txt`, 'utf-8', (err, data) => {
   let titles = data.split('\n');
-  let imdbIDs = titles.map(title => console.log(title.split(',')[1]));
-  console.log(imdbIDs);
+  let imdbIDs = titles.map(title => {
+    let tokens = title.split(' ');
+    return tokens[tokens.length - 1].replace(',', '');
+  });
+  // test on small list
+  imdbIDs = imdbIDs.slice(0,10);
+  imdbIDs.forEach(id => {
+    let baseUrl = `http://www.imdb.com/title/${id}/reviews/_ajax`;
+    console.log(baseUrl);
+    let reviewsTXT = '';
+    let paginationKey = '';
+    scrapeReviews(id, baseUrl, paginationKey, reviewsTXT, 0);
+  });
 });
+
+function scrapeReviews(id, baseUrl, paginationKey, reviewsTXT, numReviews) {
+  // console.log(baseUrl + paginationKey);
+  axios.get(baseUrl + paginationKey)
+       .then(res => {
+          let $ = cheerio.load(res.data);
+          $('.text').each((index, element) => {
+            // remove all newlines and carriage returns
+            numReviews++;
+            reviewsTXT += $(element).text().replace(/(\r\n|\n|\r)/gm,' ');
+            reviewsTXT += '\n';
+          });
+          // set paginationKey if it exists
+          paginationKey = $('.load-more-data').attr('data-key');
+          if (paginationKey) {
+            scrapeReviews(id, baseUrl, `?paginationKey=${paginationKey}`, reviewsTXT, numReviews);
+          } else {
+            reviewsTXT = `${baseUrl}\nReview Count: ${numReviews}\n${reviewsTXT}`;
+            fs.appendFile(`${reviewDirectory}/${genre}/${id}.txt`, reviewsTXT, err => {
+              if (err) { 
+                console.error(err);
+              }
+            });
+          }
+       })
+       .catch(err => console.error(err));
+
+}
 
 // function scrapeReviews(url) {
 //   axios.get(url)
