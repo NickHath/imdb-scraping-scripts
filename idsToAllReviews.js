6652ded19042581b8a497a182d4f825c0e7c31ae
Nick Hathaway <Nicholashathaway@gmail.com>
2018-02-07 15:08:08 -0500

    also captures date of publication and rating

--- idsToAllReviews.js
+++ idsToAllReviews.js
@@ -18,7 +18,7 @@ fs.readFile(`${titleDirectory}/${genre}_titles_with_ids.txt`, 'utf-8', (err, dat
     return tokens[tokens.length - 1].replace(',', '');
   });
   // test on small list
-  imdbIDs = imdbIDs.slice(0,250);
+  imdbIDs = imdbIDs.slice(0,100);
   imdbIDs.forEach((id, index) => {
     let baseUrl = `http://www.imdb.com/title/${id}/reviews/_ajax`;
     let reviewsTXT = '';
@@ -26,29 +26,33 @@ fs.readFile(`${titleDirectory}/${genre}_titles_with_ids.txt`, 'utf-8', (err, dat
     setTimeout(() => {
       console.log(`Dispatched request #${index + 1} for ${baseUrl}`);
       scrapeReviews(id, titles[index], baseUrl, paginationKey, reviewsTXT, 0);
-    }, index * 250);
-    // scrapeReviews(id, baseUrl, paginationKey, reviewsTXT, 0);
+    }, index * 175);
   });
 });
 
 function scrapeReviews(id, title, baseUrl, paginationKey, reviewsTXT, numReviews) {
-  // console.log(baseUrl + paginationKey);
   axios.get(baseUrl + paginationKey)
        .then(res => {
           let $ = cheerio.load(res.data);
-          $('.text').each((index, element) => {
+          $('.review-container').each((index, element) => {
+            let reviewHTML = $(element).html();
+            let review = cheerio.load(reviewHTML);
+
+            let title = review('.title').text();
+            let date = review('.review-date').text();
+            let text = review('.text').text();
+            let rating = review('svg + span').text();
             // remove all newlines and carriage returns
             // also get rating! -- maybe look at container itself?
             numReviews++;
-            reviewsTXT += $(element).text().replace(/(\r\n|\n|\r)/gm,' ');
-            reviewsTXT += '\n';
+            reviewsTXT += `Title: ${title}\nDate: ${date}\nRating: ${rating ? rating : 'N/A'}\n${text.replace(/(\r\n|\n|\r)/gm,' ')}\n\n`;
           });
           // set paginationKey if it exists
           paginationKey = $('.load-more-data').attr('data-key');
           if (paginationKey) {
             scrapeReviews(id, title, baseUrl, `?paginationKey=${paginationKey}`, reviewsTXT, numReviews);
           } else {
-            reviewsTXT = `${baseUrl}\n${title}\nReview Count: ${numReviews}\n${reviewsTXT}`;
+            reviewsTXT = `${baseUrl}\n${title}\nReview Count: ${numReviews}\n\n${reviewsTXT}`;
             fs.appendFile(`${reviewDirectory}/${genre}/${id}.txt`, reviewsTXT, err => {
               if (err) { 
                 console.error(err);
